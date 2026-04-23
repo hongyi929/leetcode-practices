a42c297bf4e68350da8624ce0496179d9e919697
Hong Yi <limhongyi0@gmail.com>
2026-04-23 23:14:03 +0800

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Check more or less. On found, only binary search on less.
+        n = len(piles)
+        maxNo = 0
+        minInt = None
+        for p in piles:
+            if p > maxNo:
+                maxNo = p
+        
+        l = 1
+        r = maxNo
+        count = 0
+
+        while l <= r:
+            count = 0
+            m = l + ((r-l) // 2)
+            for p in piles:
+                print(m)
+                iterations = -(p // -m)
+                count += iterations
+            
+            if count <= h:
+                if not minInt:
+                    minInt = m
+                    
+                if m < minInt:
+                    minInt = m
+                    
+                r = m - 1
+            else:
+                l = m + 1
+
+        return minInt
+        
+                
+
+
